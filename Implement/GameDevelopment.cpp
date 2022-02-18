9c69412390148d6e16027746bddca8c8e86bba92
royunsang <82229769+royunsang@users.noreply.github.com>
2022-02-19 00:05:11 +0900

    Update GameDevelopment.cpp

--- Implement/GameDevelopment.cpp
+++ Implement/GameDevelopment.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
-int arr[50][50];
-int flag[50][50];
+int arr[50][50]; //ocean
+int flag[50][50]; //visited
 
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1 };
@@ -13,63 +15,70 @@ int turnCnt = 0;
 int direction;
 
 void TurnLeft() {
-    direction = direction - 1;
-    if (direction == -1)
-    {
-        direction = 3;
-    }
+	if (direction == -1) {
+		direction = 3;
+	}
+	//3 2 1 0 
 }
 
 void Moving(int x, int y, int direction) {
-    flag[x][y] = 1;
-    TurnLeft();
-    int nx = x + dx[direction];
-    int ny = y + dy[direction];
-    if (arr[nx][ny] == 0 && flag[nx][ny] == 0) {
-        flag[nx][ny] = 1;
-        x = nx;
-        y = ny;
-        result++;
-        turnCnt = 0;
-    }
-    else turnCnt++;
+	flag[x][y] = 1;
+	TurnLeft();
+	int nx = x + dx[direction];
+	int ny = y + dy[direction];
+	if (arr[nx][ny] == 0 && flag[nx][ny] == 0) //if go forward
+	{
+		flag[nx][ny] = 1;
+		x = nx;
+		y = ny;
+		result++;
+		turnCnt = 0;
+	}
+	else turnCnt++; //if cant forward
 
-    if (turnCnt == 4) {
-        nx = x - dx[direction];
-        ny = y - dy[direction];
+	if (turnCnt == 4) {//cant move 
+		nx = x - dx[direction];
+		ny = y - dy[direction];
 
-        if (arr[nx][ny] == 0) {
-            x = nx;
-            y = ny;
-        }
-        else return;
-        turnCnt = 0;
-    }
-    Moving(nx, ny, direction);
+		if (arr[nx][ny] == 0) { //if !ocean
+			x = nx;
+			y = ny;
+			result++;
+		}
+		else return;
+		turnCnt = 0;
+	}
+
+	Moving(x, y, direction);
 }
 
+
 int main() {
+	auto startTime = std::chrono::system_clock::now();
 
-    int N, M;  //(3 <= N, M <=50)
-    cin >> N >> M;
+	int N, M; //(3 <= N, M <= 50)
+	cin >> N >> M;
 
-    int x;
-    int y;
+	int x;
+	int y;
 
-    cin >> x >> y >> direction;
+	cin >> x >> y >> direction;
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            int x;
-            cin >> x;
-            arr[i][j] = x;
-        }
-    }
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			int x;
+			cin >> x;
+			arr[i][j] = x;
+		}
+	}
 
-    Moving(x, y, direction);
-    cout << result;
-
-    return 0;
+	Moving(x, y, direction);
+	cout << result << endl;
+	auto endTime = std::chrono::system_clock::now();
+	auto sec = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+	
+	cout << sec.count();
+	return 0;
 }
