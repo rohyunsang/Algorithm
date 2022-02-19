101ea682cac56fca702e0c8f3865d012d96b7556
royunsang <82229769+royunsang@users.noreply.github.com>
2022-02-19 22:42:47 +0900

    Update MiroEscape.cpp

--- BFS/MiroEscape.cpp
+++ BFS/MiroEscape.cpp
@@ -3,31 +3,20 @@
 //queue push add last element 
 //queue pop delete first element
 
-
 using namespace std;
 //BFS is guaranteed optimal solution
+
 int arr[201][201];
 int N, M;
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,-1,0,1 };
 
-/* bool IsSafe(int x, int y)
-{
-	if ((0 <= x && x < M) && (0 <= y && y <= N))
-	{
-		return true;
-	}
-	return false;
-}*/
-
 int BFS(int x, int y)
 {
-	cout << "start";
 	queue<pair<int, int>> q;
 	q.push({ x,y });
 
 	while (!q.empty()) { // queue is empty 
-		cout << "start";
 		int x = q.front().first;
 		int y = q.front().second;
 		q.pop();
@@ -37,12 +26,15 @@ int BFS(int x, int y)
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if ((nx < 0 || nx >= N || ny < 0 || ny >= M)) continue;
+			if ((nx < 0 || nx >= N || ny < 0 || ny >= M)) {
+				continue;
+			}
 
-			if (arr[nx][ny] == 0) continue;
+			if (arr[nx][ny] == 0) {
+				continue;
+			}
 
 			if (arr[nx][ny] == 1) {
-				cout << "move";
 				arr[nx][ny] = arr[x][y] + 1;
 				q.push({ nx,ny });
 			}
@@ -59,8 +51,7 @@ int main(void)
 	{
 		for (int j = 0; j < M; j++)
 		{
-			cin >> arr[i][j];
-			cout << "for";
+			scanf_s("%1d", &arr[i][j]);
 		}
 	}
 
