9bdeb9ce7f9a9fb769bd5935363ab046b10bdf54
royunsang <82229769+royunsang@users.noreply.github.com>
2022-02-19 15:23:32 +0900

    Update GameDevelopment.cpp

--- Implement/GameDevelopment.cpp
+++ Implement/GameDevelopment.cpp
@@ -8,13 +8,14 @@ int arr[50][50]; //ocean
 int flag[50][50]; //visited
 
 int dx[] = { -1,0,1,0 };
-int dy[] = { 0,1,0,-1 };
+int dy[] = { 0,1,0,-1 };  
 
 int result = 0;
 int turnCnt = 0;
 int direction;
 
 void TurnLeft() {
+	direction = direction - 1;
 	if (direction == -1) {
 		direction = 3;
 	}
@@ -22,33 +23,40 @@ void TurnLeft() {
 }
 
 void Moving(int x, int y, int direction) {
+	cout << "a";
 	flag[x][y] = 1;
 	TurnLeft();
 	int nx = x + dx[direction];
 	int ny = y + dy[direction];
 	if (arr[nx][ny] == 0 && flag[nx][ny] == 0) //if go forward
 	{
+		cout << "b";
 		flag[nx][ny] = 1;
 		x = nx;
 		y = ny;
 		result++;
 		turnCnt = 0;
 	}
-	else turnCnt++; //if cant forward
+	else {
+		turnCnt++;
+		cout << "c";
+	}//if cant forward
 
 	if (turnCnt == 4) {//cant move 
+		cout << "d";
 		nx = x - dx[direction];
 		ny = y - dy[direction];
 
 		if (arr[nx][ny] == 0) { //if !ocean
 			x = nx;
 			y = ny;
-			result++;
+			cout << "e";
 		}
-		else return;
+		else return; //if ocean return ;
+
 		turnCnt = 0;
 	}
-
+	cout << "f";
 	Moving(x, y, direction);
 }
 
@@ -71,6 +79,7 @@ int main() {
 			int x;
 			cin >> x;
 			arr[i][j] = x;
+			flag[i][j] = 0;
 		}
 	}
 
@@ -78,7 +87,7 @@ int main() {
 	cout << result << endl;
 	auto endTime = std::chrono::system_clock::now();
 	auto sec = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-	
+
 	cout << sec.count();
 	return 0;
 }
