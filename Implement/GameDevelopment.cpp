f604a83e15e8ebed695a761cf8c477c7bf63cd8e
royunsang <82229769+royunsang@users.noreply.github.com>
2022-02-19 15:35:44 +0900

    Update GameDevelopment.cpp

--- Implement/GameDevelopment.cpp
+++ Implement/GameDevelopment.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
-#include <chrono>
 
 using namespace std;
-using namespace chrono;
 
 int arr[50][50]; //ocean
 int flag[50][50]; //visited
 
 int dx[] = { -1,0,1,0 };
-int dy[] = { 0,1,0,-1 };  
+int dy[] = { 0,1,0,-1 };
 
-int result = 0;
+int result = 1;
 int turnCnt = 0;
-int direction;
+int dir;
 
-void TurnLeft() {
-	direction = direction - 1;
-	if (direction == -1) {
-		direction = 3;
-	}
-	//3 2 1 0 
-}
-
-void Moving(int x, int y, int direction) {
-	cout << "a";
+void Moving(int x, int y, int dir) {
+	
 	flag[x][y] = 1;
-	TurnLeft();
-	int nx = x + dx[direction];
-	int ny = y + dy[direction];
+	dir = dir - 1;
+	if (dir == -1)
+	{
+		dir = 3;
+	}
+	
+	int nx = x + dx[dir];
+	int ny = y + dy[dir];
 	if (arr[nx][ny] == 0 && flag[nx][ny] == 0) //if go forward
 	{
-		cout << "b";
+		
 		flag[nx][ny] = 1;
 		x = nx;
 		y = ny;
@@ -39,30 +34,30 @@ void Moving(int x, int y, int direction) {
 	}
 	else {
 		turnCnt++;
-		cout << "c";
+		
 	}//if cant forward
 
 	if (turnCnt == 4) {//cant move 
-		cout << "d";
-		nx = x - dx[direction];
-		ny = y - dy[direction];
+		
+		nx = x - dx[dir];
+		ny = y - dy[dir];
 
 		if (arr[nx][ny] == 0) { //if !ocean
 			x = nx;
 			y = ny;
-			cout << "e";
+			
 		}
 		else return; //if ocean return ;
 
 		turnCnt = 0;
 	}
-	cout << "f";
-	Moving(x, y, direction);
+	
+	Moving(x, y, dir);
 }
 
 
 int main() {
-	auto startTime = std::chrono::system_clock::now();
+
 
 	int N, M; //(3 <= N, M <= 50)
 	cin >> N >> M;
@@ -70,7 +65,7 @@ int main() {
 	int x;
 	int y;
 
-	cin >> x >> y >> direction;
+	cin >> x >> y >> dir;
 
 	for (int i = 0; i < N; i++)
 	{
@@ -83,11 +78,8 @@ int main() {
 		}
 	}
 
-	Moving(x, y, direction);
+	Moving(x, y, dir);
 	cout << result << endl;
-	auto endTime = std::chrono::system_clock::now();
-	auto sec = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
-	cout << sec.count();
 	return 0;
 }
