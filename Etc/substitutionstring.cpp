993be71c20d0a4af92816474f392766a071ba07a
Roh <82229769+rohyunsang@users.noreply.github.com>
2022-06-20 04:05:58 +0900

    Update substitutionstring.cpp

--- Etc/substitutionstring.cpp
+++ Etc/substitutionstring.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h> //strstr 함수가 선언된 헤더 파일
+#include <cstring> //c++일경우
+int main()
+{
+    char str[] = "This is a simple string";
+    char* ptr = strchr(str, 'p');  // p가 존재하는 위치를 찾음
+    if (ptr) {
+        *ptr = 't';  // t로 변경
+        puts(str);
+    }
 
+    return 0;
+}
