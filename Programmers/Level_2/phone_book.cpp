197c7c23e7a65961ff8646d6d10e2f628d454eb2
Roh <82229769+rohyunsang@users.noreply.github.com>
2022-08-11 22:36:04 +0900

    Create phone_book.cpp

---
+++ Programmers/Level_2/phone_book.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> p) {
+
+    sort(p.begin(),p.end());
+
+    for(int i = 0 ; i < p.size() - 1; i++)
+    {
+        if(p[i] == p[i+1].substr(0,p[i].size()))
+            return false;
+    }
+
+
+    return true;
+}
+
+
+// substr 
+// find 
