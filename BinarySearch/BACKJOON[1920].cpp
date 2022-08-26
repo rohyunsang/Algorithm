1f6d7ae02f7b1d96ca1e05909256b1219e706acb
royunsang <82229769+rohyunsang@users.noreply.github.com>
2022-08-26 22:49:52 +0900

    Create BACKJOON[1920].cpp

---
+++ BinarySearch/BACKJOON[1920].cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[100010];
+
+void binarySearch(int key) {
+	int start = 0;
+	int end = N - 1;
+	int mid;
+
+	while (end >= start) {
+		mid = (start + end) / 2;
+		if (arr[mid] == key) {
+			cout << 1 << "\n";
+			return;
+		}
+		else if (arr[mid] > key) {
+			end = mid - 1;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+	cout << 0 << "\n";
+	return;
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	int tmp;
+
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		arr[i] = tmp;
+	}
+
+	sort(arr, arr + N);
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		cin >> tmp;
+		binarySearch(tmp);
+	}
+
+	
+	return 0;
+}
