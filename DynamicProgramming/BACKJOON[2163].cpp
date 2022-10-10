60d5064203e87a108bdd30fc192a4c34c929df01
Roh <82229769+rohyunsang@users.noreply.github.com>
2022-10-10 16:50:49 +0900

    Add files via upload

---
+++ DynamicProgramming/BACKJOON[2163].cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+// けけ    け け 
+// けけ    け け
+
+// けけけ  け け け
+// けけけ  け け け
+
+int main() {
+	cin >> n >> m;
+	cout << n * m - 1;
+
+	return 0;
+}
