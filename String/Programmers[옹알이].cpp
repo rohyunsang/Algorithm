35aab839dc66573915a43c758b3d403fe2f1fa4c
Roh <82229769+rohyunsang@users.noreply.github.com>
2022-11-18 00:03:30 +0900

    Create Programmers[옹알이].cpp

---
+++ String/Programmers[옹알이].cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+// aya ye woo ma 
+
+vector<string> v;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    
+    for (int i = 0; i < babbling.size(); i++) {
+        bool check = false;
+
+        for (int j = 0; j < babbling[i].size(); j++) {
+            if (babbling[i].substr(j, 3) == "aya") j += 2;
+            else if (babbling[i].substr(j, 2) == "ye") j += 1;
+            else if (babbling[i].substr(j, 3) == "woo") j += 2;
+            else if (babbling[i].substr(j, 2) == "ma") j += 1;
+            else {
+                check = true;
+                break;
+            }
+        }
+        if (!check) answer++;
+    }
+
+    return answer;
+}
