88e49989e2127196dace3c5c5a7802d6f33a86f0
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-01-25 19:56:33 +0900

    Create P[땅따먹기].cpp

---
+++ DynamicProgramming/P[땅따먹기].cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//Dynamic Programming 
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+
+    for (int i = 0; i < land.size() - 1; i++) {
+        land[i + 1][0] += max(land[i][1], max(land[i][2], land[i][3]));
+        land[i + 1][1] += max(land[i][0], max(land[i][2], land[i][3]));
+        land[i + 1][2] += max(land[i][0], max(land[i][1], land[i][3]));
+        land[i + 1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+    }
+
+    for (int i = 0; i < 4; i++) {
+        answer = max(land[land.size() - 1][i], answer);
+    }
+
+
+    return answer;
+}
