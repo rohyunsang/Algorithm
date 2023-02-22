b728f1d46db36f79fb2b5c795fb58060ad81d80a
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-02-22 16:46:42 +0900

    Create BJ[9656].cpp

---
+++ GameTheory/BJ[9656].cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (n % 2 == 1)
+		cout << "CY";
+	else
+		cout << "SK";
+
+	return 0;
+}
+
+//1 sk lose
+//2 cy lose
+//3 sk lose
+//4 cy lose
