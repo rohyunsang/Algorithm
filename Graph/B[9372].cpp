b83534c7e1f6f2078d3c2fb3c5c2eb2f101c992f
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-03-14 13:51:57 +0900

    Create B[9372].cpp

---
+++ Graph/B[9372].cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int t;
+int n, m;
+
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> n >> m;
+		for (int i = 0; i < m; i++){
+			int nTmp1, nTmp2;
+			cin >> nTmp1 >> nTmp2;
+		}
+		cout << n - 1 << endl;
+	}
+
+
+	return 0;
+}
