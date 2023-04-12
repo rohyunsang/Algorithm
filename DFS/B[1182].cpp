8804790436ede998ff96ba002cfb399da143c79a
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-04-12 21:05:51 +0900

    Update B[1182].cpp

--- DFS/B[1182].cpp
+++ DFS/B[1182].cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int n, s;
-int ans = 0;
-
-// dfs 
-// backtracking? 
+int n;
+int s;
+int cnt = 0; 
 
 vector<int> v;
 
 void dfs(int idx, int tmp) {
-	if (idx == n)
-		return;
-	if (tmp + v[idx] == s)
-		ans++;
+	if (idx == n) return;
+	if (tmp + v[idx] == s) cnt++;
+
 	dfs(idx + 1, tmp);
 	dfs(idx + 1, tmp + v[idx]);
 }
 
 int main() {
 	cin >> n >> s;
-	while (n--) {
+	for (int i = 0; i < n; i++) {
 		int nTmp;
 		cin >> nTmp;
 		v.push_back(nTmp);
 	}
+	dfs(0, 0);
+	cout << cnt;
+
+	/*do {
+		for (int i = 0; i < 3; i++)
+			cout << v[i] << " ";
+		cout << '\n';
+
+	} while (next_permutation(v.begin(), v.end()));*/
 
 
 	return 0;
